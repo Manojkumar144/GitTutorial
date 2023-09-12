68eaa1f29816b7042000087c17864547d9ee8d0e
Manojkumar144 <manojsantha1442000@gmail.com>
2023-09-12 16:43:09 +0530

    querySelector changes

--- practice.js
+++ practice.js
@@ -1,24 +1,40 @@
 
-var header = document.getElementById('main-header');
+// var header = document.getElementById('main-header');
 
-var addItemsTitle = document.querySelector('.title'); 
-addItemsTitle.style.color = 'green';
-header.style.borderBottom = 'solid 3px #000';
+// var addItemsTitle = document.querySelector('.title'); 
+// addItemsTitle.style.color = 'green';
+// header.style.borderBottom = 'solid 3px #000';
 
 
-var items = document.getElementsByClassName('list-group-item');
+// var items = document.getElementsByClassName('list-group-item');
 
-items[2].style.color = 'green';
+// items[2].style.color = 'green';
 
-for (var i = 0; i < items.length; i++) {
-  items[i].style.backgroundColor = 'red';
-  items[i].style.fontWeight = 'bold';
+// for (var i = 0; i < items.length; i++) {
+//   items[i].style.backgroundColor = 'red';
+//   items[i].style.fontWeight = 'bold';
+// }
+
+// var listItems = document.getElementsByClassName('list-group-lastItem');
+// listItems[0].style.backgroundColor = 'red';
+// listItems[0].style.fontWeight = 'bold';
+
+// var li=document.getElementsByTagName('li');
+// li[4].textContent='Hello';
+// li[4].style.backgroundColor="grey";
+
+//QuerySelector
+
+var header= document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
+
+//QuerySelectorAll
+var firstLi = document.querySelector('li:nth-child(1)');
+firstLi.style.color = 'green';
+
+// Select all odd-numbered <li> elements and change their background color to green
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for (var i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = 'green';
 }
-
-var listItems = document.getElementsByClassName('list-group-lastItem');
-listItems[0].style.backgroundColor = 'red';
-listItems[0].style.fontWeight = 'bold';
-
-var li=document.getElementsByTagName('li');
-li[4].textContent='Hello';
-li[4].style.backgroundColor="grey";
