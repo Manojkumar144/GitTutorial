4deb124235525c7ecb1b27c2a7482b4bb8664afc
Manojkumar144 <manojsantha1442000@gmail.com>
2023-09-12 16:00:36 +0530

    Added New List Item

--- practice.js
+++ practice.js
@@ -14,3 +14,11 @@ for (var i = 0; i < items.length; i++) {
   items[i].style.backgroundColor = 'red';
   items[i].style.fontWeight = 'bold';
 }
+
+var listItems = document.getElementsByClassName('list-group-lastItem');
+listItems[0].style.backgroundColor = 'red';
+listItems[0].style.fontWeight = 'bold';
+
+var li=document.getElementsByTagName('li');
+li[4].textContent='Hello';
+li[4].style.backgroundColor="grey";
