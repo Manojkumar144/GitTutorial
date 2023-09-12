dc7e7b8f487a7afced5872337b0768323a06d063
Manojkumar144 <manojsantha1442000@gmail.com>
2023-09-12 15:15:12 +0530

    DomChanges

---
+++ practice.js
@@ -0,0 +1,6 @@
+
+var header = document.getElementById('main-header');
+
+var addItemsTitle = document.querySelector('.title'); 
+addItemsTitle.style.color = 'green';
+header.style.borderBottom = 'solid 3px #000';
