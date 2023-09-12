755fb3ec1d6c506ebeb6ac2695500f76fec4b9d3
Manojkumar144 <manojsantha1442000@gmail.com>
2023-09-12 15:43:08 +0530

    ColorUpdated

--- practice.js
+++ practice.js
@@ -4,3 +4,13 @@ var header = document.getElementById('main-header');
 var addItemsTitle = document.querySelector('.title'); 
 addItemsTitle.style.color = 'green';
 header.style.borderBottom = 'solid 3px #000';
+
+
+var items = document.getElementsByClassName('list-group-item');
+
+items[2].style.color = 'green';
+
+for (var i = 0; i < items.length; i++) {
+  items[i].style.backgroundColor = 'red';
+  items[i].style.fontWeight = 'bold';
+}
