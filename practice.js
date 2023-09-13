fe8cb4fa68521e05d09c04ded75cb8238fead52b
Manojkumar144 <manojsantha1442000@gmail.com>
2023-09-13 12:27:48 +0530

    added filters and description

--- practice.js
+++ practice.js
@@ -112,19 +112,25 @@
 
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
-
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
 // Add item
 function addItem(e){
   e.preventDefault();
 
   // Get input value
-  var newItem = document.getElementById('item').value;
+  var newItem= document.getElementById('item').value;
+  var newItem1=document.getElementById('item1').value;
+
+  newItem=newItem+"\n"+newItem1;
 
   // Create new li element
   var li = document.createElement('li');
@@ -171,5 +177,22 @@ function removeItem(e){
     }
   }
 }
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
 
+  
 
